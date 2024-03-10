eae2cb269b59f3217a623a0096201a1ce4f069f9
samit1719 <samuelteshome1719@gmail.com>
2024-03-10 23:20:40 +0300

    working on conversation

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,22 @@
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
+import useConversation  from "../../zustand/useConversation";
 
 const MessageContainer = () => {
-  const noChatSelected = false;
+
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* header */}
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John doe</span>
+            <span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
           </div>
 
           {/* messages */}
